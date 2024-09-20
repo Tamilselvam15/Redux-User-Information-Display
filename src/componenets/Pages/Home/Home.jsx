a0046498627e5c5474c405cd70eb13c352b37cd0
Tamilselvam15 <tamil2002selvam@gmail.com>
2024-09-20 12:59:28 +0530

    button removed in home page

--- src/componenets/Pages/Home/Home.jsx
+++ src/componenets/Pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import './Home.css'
-import { Button } from 'reactstrap'
+// import { Button } from 'reactstrap'
 const Home = () => {
   return (
     <div className='homes' >
-      <Button color="primary" className='button'
-        outline>Get Users</Button>
+      {/* <Button color="primary" className='button'
+        outline>Get Users</Button> */}
+      <h1>Hello Guys...Welcome to our Page </h1>
       
     </div>
   )
